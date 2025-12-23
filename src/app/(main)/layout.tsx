a6b496d718ed70c78e418883a4ee78791bafd5cd
unexCoder <unexCoder@unexCoder.local>
2025-12-23 12:44:31 -0300

    metadata improvement

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,8 +3,61 @@ import Footer from "../components/layout/Footer";
 import { NavigationProvider } from "../context/NavigationContext";
 
 export const metadata: Metadata = {
-  title: "Festival Cluster",
-  description: "Encuentro de creatividad y transformación digital",
+  title: {
+    default: "Festival Cluster",
+    template: "%s | Festival Clusterr"
+  },
+  description: "Festival Tecnológico | Encuentro de creatividad y transformación digital",
+  keywords: ["festival","creatividad tecnológica", "desarrollo web", "innovación digital", "tecnología", "microCluster"],
+  authors: [{ name: "Luigi Tamagnini" }],
+  creator: "Luigi Tamagnini",
+  openGraph: {
+    type: "website",
+    locale: "es_AR",
+    url: "https://festivalcluster.org/microcluster",
+    siteName: "Festival Cluster",
+    title: "Festival Cluster",
+    description: "Festival Tecnológico. Encuentro de creatividad y transformación digital",
+    images: [
+      {
+        url: "/og-image.jpg", // Imagen principal
+        width: 1200,
+        height: 630,
+        alt: "Festival Cluster - Festival Tecnológico",
+        type: "image/jpeg"
+      },
+      {
+        url: "/og-image-w.jpg", // Imagen cuadrada para WhatsApp
+        width: 630,
+        height: 630,
+        alt: "Festival Cluster"
+      }
+    ]
+  },
+  // Twitter/X
+  twitter: {
+    card: "summary_large_image",
+    title: "Festival Cluster - Festival Tecnológico",
+    description: "Festival Tecnológico. Encuentro de creatividad y transformación digital",
+    images: ["/og-image.jpg"],
+    creator: "@unexcoder",
+    site: "@unexcoder"
+  },
+  // Otras propiedades importantes
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    }
+  },
+  
+  alternates: {
+    canonical: "https://festivalcluster.org"
+  }
 };
 
 export default function MainLayout({
--- src/app/(main)/microcluster/page.tsx
+++ src/app/(main)/microcluster/page.tsx
@@ -5,8 +5,61 @@ import Fundacion from "../../components/fundacion/FundacionMicroCluster"
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "microCluster",
+  title: {
+    default: "microCluster - Plataforma de Creatividad Tecnológica",
+    template: "%s | microCluster"
+  },
   description: "Plataforma de creatividad tecnológica",
+  keywords: ["creatividad tecnológica", "desarrollo web", "innovación digital", "tecnología", "microCluster"],
+  authors: [{ name: "LuigiTamagnini" }],
+  creator: "Luigi Tamagnini",
+  openGraph: {
+    type: "website",
+    locale: "es_AR",
+    url: "https://festivalcluster.org/microcluster",
+    siteName: "microCluster",
+    title: "microCluster - Plataforma de Creatividad Tecnológica",
+    description: "Plataforma innovadora de creatividad tecnológica e innovación en cultura digital",
+    images: [
+      {
+        url: "/og-image-mc.jpg", // Imagen principal
+        width: 1200,
+        height: 630,
+        alt: "microCluster - Plataforma de Creatividad Tecnológica",
+        type: "image/jpeg"
+      },
+      {
+        url: "/og-image-mc-w.jpg", // Imagen cuadrada para WhatsApp
+        width: 630,
+        height: 630,
+        alt: "microCluster"
+      }
+    ]
+  },
+  // Twitter/X
+  twitter: {
+    card: "summary_large_image",
+    title: "microCluster - Plataforma de Creatividad Tecnológica",
+    description: "Plataforma innovadora de creatividad tecnológica",
+    images: ["/og-image.jpg"],
+    creator: "@unexcoder",
+    site: "@unexcoder"
+  },
+  // Otras propiedades importantes
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    }
+  },
+  
+  alternates: {
+    canonical: "https://festivalcluster.org"
+  }
 };
 
 export default function page() {
